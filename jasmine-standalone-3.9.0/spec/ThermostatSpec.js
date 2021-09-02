4f5e4d0ad7cba98261880bebafaf7cb1aef5faed
Corinne <corinne.boesch@gmail.com>
2021-09-02 15:11:40 +0100

    addint constants as magic number

--- jasmine-standalone-3.9.0/spec/ThermostatSpec.js
+++ jasmine-standalone-3.9.0/spec/ThermostatSpec.js
@@ -1,66 +1,65 @@
-describe("Thermostat", function() {
-  
+describe('Thermostat', function () {
   let thermostat;
 
-  beforeEach(function() {
+  beforeEach(function () {
     thermostat = new Thermostat();
   });
 
-  describe('any new instances of thermostat class have temperature set to 20', function() { 
-    it("temperature is 20 for thermostat instance", function() {
-      expect(thermostat.temperature).toEqual(20)
-    })
-  })
+  describe('any new instances of thermostat class have temperature set to 20', function () {
+    it('temperature is 20 for thermostat instance', function () {
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
 
-  describe('increase temperature by value of argument passed in', function() { 
-    it("temperature increases by 10 degrees", function() {
+  describe('increase temperature by value of argument passed in', function () {
+    it('temperature increases by 10 degrees', function () {
       thermostat.togglePowerSaveMode();
       thermostat.increaseTemperature(10);
-      expect(thermostat.temperature).toEqual(30)
-    })
-  })
+      expect(thermostat.temperature).toEqual(30);
+    });
+  });
 
-  describe('decrease temperature by value of argument passed in', function() { 
-    it("temperature decreases by 5 degrees", function() {
+  describe('decrease temperature by value of argument passed in', function () {
+    it('temperature decreases by 5 degrees', function () {
       thermostat.decreaseTemperature(5);
-      expect(thermostat.temperature).toEqual(15)
-    })
-  })
-  
-  describe('minimum temperature', function() { 
-    it("should not go below 10", function() {
+      expect(thermostat.temperature).toEqual(15);
+    });
+  });
+
+  describe('minimum temperature', function () {
+    it('should not go below 10', function () {
       thermostat.decreaseTemperature(20);
-      expect(thermostat.temperature).toEqual(10)
-    })
-  })
+      expect(thermostat.temperature).toEqual(10);
+    });
+  });
 
-  describe('max temperature when power saving mode is on', function() { 
-    it("should not go above 25", function() {
+  describe('max temperature when power saving mode is on', function () {
+    it('should not go above 25', function () {
       thermostat.increaseTemperature(50);
-      expect(thermostat.temperature).toEqual(25)
-    })
-  })
+      expect(thermostat.temperature).toEqual(25);
+    });
+  });
 
-  describe('max temperature when power saving mode is off', function() { 
-    it("should not go above 32", function() {
+  describe('max temperature when power saving mode is off', function () {
+    it('should not go above 32', function () {
       thermostat.togglePowerSaveMode();
       thermostat.increaseTemperature(50);
-      expect(thermostat.temperature).toEqual(32)
-    })
-  })
+      expect(thermostat.temperature).toEqual(32);
+    });
+  });
 
-  describe('powersaving mode is on by default but can be turned off', function() { 
-    it("should turn off", function() {
+  describe('powersaving mode is on by default but can be turned off', function () {
+    it('should turn off', function () {
       thermostat.togglePowerSaveMode();
-      expect(thermostat.powerSaveMode).toEqual("off")
-    })
-  })
+      expect(thermostat.powerSaveMode).toEqual('off');
+    });
+  });
 
-  describe('function that resets temperature to 20', function() { 
-    it("changes temperature to 20", function() {
+  describe('function that resets temperature to 20', function () {
+    it('changes temperature to 20', function () {
       thermostat.increaseTemperature(70);
       thermostat.resetTemperature();
-      expect(thermostat.temperature).toEqual(20)
-    })
-  })
-})
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+});
--- jasmine-standalone-3.9.0/src/Thermostat.js
+++ jasmine-standalone-3.9.0/src/Thermostat.js
@@ -1,44 +1,57 @@
 class Thermostat {
   constructor() {
-    this.minTemperature = 10;
-    this.temperature = 20
-    this.powerSaveMode = "on"
+    this.MAX_TEMP_ON = 25;
+    this.MAX_TEMP_OFF = 32;
+    this.MIN_TEMP = 10;
+    this.minTemperature = this.MIN_TEMP;
+    this.START_TEMP = 20;
+    this.temperature = this.START_TEMP;
+    this.powerSaveMode = 'on';
   }
 
   increaseTemperature(number) {
-    this.temperature += number
-    this.checkTemperature()
+    this.temperature += number;
+    this._checkTemperature();
   }
 
   decreaseTemperature(number) {
     this.temperature -= number;
-    this.checkTemperature();
+    this._checkTemperature();
   }
 
-  checkTemperature(){
-    if (this.temperature < this.minTemperature) {this.temperature = 10;}
-    else if (this.isPowerSaveModeOn() && this.temperature > 25) {this.temperature = 25}
-    else if (!this.isPowerSaveModeOn() && this.temperature > 32) {this.temperature = 32}
-  }
-
-  isPowerSaveModeOn() {
-    if (this.powerSaveMode === "on") { return true }
-    else {return false;}
-  }
-
-  togglePowerSaveMode () {
-    if (this.powerSaveMode === "off") { this.powerSaveMode = "on" }
-    else if (this.powerSaveMode === "on") { this.powerSaveMode = "off" }
+  togglePowerSaveMode() {
+    if (this.powerSaveMode === 'off') {
+      this.powerSaveMode = 'on';
+    } else if (this.powerSaveMode === 'on') {
+      this.powerSaveMode = 'off';
+    }
   }
 
   resetTemperature() {
-    this.temperature = 20;
+    this.temperature = this.START_TEMP;
+  }
+
+  _isPowerSaveModeOn() {
+    if (this.powerSaveMode === 'on') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  _checkTemperature() {
+    if (this.temperature < this.minTemperature) {
+      this.temperature = this.MIN_TEMP;
+    } else if (
+      this._isPowerSaveModeOn() &&
+      this.temperature > this.MAX_TEMP_ON
+    ) {
+      this.temperature = this.MAX_TEMP_ON;
+    } else if (
+      !this._isPowerSaveModeOn() &&
+      this.temperature > this.MAX_TEMP_OFF
+    ) {
+      this.temperature = this.MAX_TEMP_OFF;
+    }
   }
 }
-
-// completed reset function
-
-
-
-
-
