7b40428d070b618c7041f9099d46903cf78ad000
Corinne <corinne.boesch@gmail.com>
2021-09-02 17:30:02 +0100

    nested interfae functions

--- jasmine-standalone-3.9.0/src/Interface.js
+++ jasmine-standalone-3.9.0/src/Interface.js
@@ -1,12 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
+  const updateTemperature = () => {
+    document.querySelector('#temperature').innerText = thermostat.temperature;
+  };
+
   const thermostat = new Thermostat();
-  document.querySelector('#temperature').innerText = thermostat.temperature
-})
+  updateTemperature();
 
-const thermostat = new Thermostat();
-document.querySelector('#increase-temp').addEventListener('click', () => {
-  // const thermostat = new Thermostat();
-  thermostat.increaseTemperature(1);
-  document.querySelector('#temperature').innerText = thermostat.temperature;
-})
+  document.querySelector('#increase-temp').addEventListener('click', () => {
+    thermostat.increaseTemperature(1);
+    document.querySelector('#temperature').innerText = thermostat.temperature;
+  });
 
+  document.querySelector('#decrease-temp').addEventListener('click', () => {
+    // const thermostat = new Thermostat();
+    thermostat.decreaseTemperature(1);
+    document.querySelector('#temperature').innerText = thermostat.temperature;
+  });
+});
